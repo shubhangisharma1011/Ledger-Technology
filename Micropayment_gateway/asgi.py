1c34693a46e4b550e2c5c2b05e1a3c44f1143627
shubhangisharma1011 <shubhangisharma088@gmail.com>
2022-04-15 11:31:03 +0530

    Ledger Technology

---
+++ Micropayment_gateway/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Micropayment_gateway project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Micropayment_gateway.settings')
+
+application = get_asgi_application()
